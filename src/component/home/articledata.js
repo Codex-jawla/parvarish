b45a79606e36ac2ad4814396875bd7bed78c4275
HIMANSHU JAWLA <101585686+Codex-jawla@users.noreply.github.com>
2023-10-02 23:39:32 +0530

    Add files via upload

---
+++ src/component/home/articledata.js
@@ -0,0 +1,17 @@
+
+const article=[
+    {imgsrc : "https://images.assettype.com/fortuneindia%2F2022-09%2F305e0299-2791-4cf0-a2aa-97492d492bcb%2FRich_list__Tata_1.jpg?rect=0,8,1908,1073&w=1250&q=60",
+    content : "बातें जो विद्यार्थी को नहीं सिखाई जाती : रतन टाटा की सीख",
+    btnlink : "",
+    },
+    {imgsrc : "https://discoverymood.com/wp-content/uploads/2019/05/iStock-949202990.jpg",
+    content : "Are you a perfectionist? Do you have perfection syndrome?",
+    btnlink : "",
+    },
+    {imgsrc : "https://www.governancenow.com/temp/electricity21.jpg",
+    content : "50 हजार का बिल देख पंचायत खुद ही बिजली बनाने लगी",
+    btnlink : "",
+    },
+]
+
+export default article;
---
+++ src/component/home/blogdata.js
@@ -0,0 +1,12 @@
+const blogs=[
+    {imgsrc : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfpRtsM0Nq7RQ7pGuNY4mjDn0ifRzRT_bWFQ&usqp=CAU",
+    content : "नींबू घास की खेती कैसे करें",
+    btnlink : ""},
+    {imgsrc : "https://c.ndtvimg.com/2022-04/0j6j529o_pariksha-pe-charcha_625x300_01_April_22.jpg",
+    content : "परीक्षा की चर्चा: विद्यार्थियों को परीक्षा की तनाव रहित तैयारी के सूत्र ",
+    btnlink : ""},
+    {imgsrc : "https://i0.wp.com/spiralofbliss.com/wp-content/uploads/2017/05/birdsofafeatheretsy3.jpg?w=1600&ssl=1",
+    content : "कोई दोस्त क्यों नहीं मिलता : विजय गोयल",
+    btnlink : "/gallery"},
+]
+export default blogs;
